c40c6b825d5e4b28aee3a3f22d192a04c6a303fa
Shawn Kost <shawn.kost@twisted-rope.com>
2022-01-18 00:28:38 -0800

    Update background & show Home component

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,22 @@
   <v-app>
     <Header />
 
-    <v-main>
-      <HelloWorld />
+    <v-main class="grey darken-4">
+      <Home />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 import Header from './components/Header';
+import Home from './views/Home';
+
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
     Header,
+    Home
   },
 
   data: () => ({
@@ -31,4 +32,5 @@ export default {
   src: url('./fonts/LeagueSpartan-Bold.otf') format('opentype');
 }
 @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap');
+
 </style>
