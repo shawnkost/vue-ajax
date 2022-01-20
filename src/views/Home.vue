bd72a6cb4dd0de1f37c256d03c0b4a7ae0c9ccd4
Shawn Kost <shawn.kost@twisted-rope.com>
2022-01-19 23:01:53 -0800

    Render TopAiringAnime

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <TopAnime />
+  <v-container>
+    <TopAnime />
+    <TopAiringAnime />
+  </v-container>
 </template>
 
 <script>
 import TopAnime from '../components/TopAnime';
+import TopAiringAnime from '../components/TopAiringAnime';
 
 export default {
   name: 'Home',
 
   components: {
     TopAnime,
+    TopAiringAnime,
   },
 };
 </script>
