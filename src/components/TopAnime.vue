349c44b567a08a513f606cfcdb20c94aa29cc4e3
Shawn Kost <shawn.kost@twisted-rope.com>
2022-01-19 22:09:45 -0800

    Update TopAnime to make api request after 4 hours

--- src/components/TopAnime.vue
+++ src/components/TopAnime.vue
@@ -2,9 +2,9 @@
   <v-container>
     <h1>Top Anime</h1>
     <v-sheet dark class="sheet">
-      <v-slide-group class="py-4 pr-4" center-active show-arrows>
+      <v-slide-group class="py-4 pr-4" show-arrows>
         <v-slide-item v-for="show in shows" :key="show.id">
-          <v-card class="ma-2 anime-card" width="300px" light>
+          <v-card class="ma-2 anime-card" :width="$vuetify.breakpoint.sm ? '20%' : '50%'" light>
             <v-img class="align-end" max-height="400px" :src="show.image_url">
             </v-img>
 
@@ -37,19 +37,23 @@ export default {
       let data = localStorage.getItem('topAnime');
       data = JSON.parse(data);
 
-      if (!data) {
+      if (!data.shows || this.isDataOutdated(data)) {
         try {
           let response = await fetch(`https://api.jikan.moe/v3/top/anime`);
           let temp = await response.json();
-          localStorage.setItem('topAnime', JSON.stringify(temp));
+          localStorage.setItem('topAnime', JSON.stringify({shows: temp.top, receivedAt: new Date()}));
           this.shows = temp.top;
         } catch (error) {
           console.log(error);
         }
       } else {
-        this.shows = data.top;
+        this.shows = data.shows;
       }
     },
+    isDataOutdated(receivedAt) {
+      const checkDate = new Date(new Date().getTime() - (60 * 60 * 4 * 1000));
+      return new Date(receivedAt).getTime() < checkDate.getTime();
+    }
   },
 
   created() {
@@ -63,7 +67,7 @@ h1 {
   color: white;
   font-family: 'League Spartan', sans-serif;
   font-size: 48px;
-  margin-bottom: 20px;
+  margin: 0px 0px 20px 50px;
 }
 
 .sheet {
