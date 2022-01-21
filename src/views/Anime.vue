42699f9cf31f3d65d3482d97819489f3f736204a
Shawn Kost <shawn.kost@twisted-rope.com>
2022-01-20 18:36:49 -0800

    WIP: Create Anime component

---
+++ src/views/Anime.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Test</h1>
+</template>
+
+<script>
+export default {
+  name: 'Anime',
+
+  props: {
+    malId: Number,
+  },
+
+  methods: {
+    async getData() {
+      console.log('anime', this.malId);
+      try {
+        let response = await fetch(
+          `https://api.jikan.moe/v3/anime/${this.malId}`
+        );
+        let temp = await response.json();
+        console.log('temp', temp);
+        // this.shows = temp.top;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  mounted() {
+    this.getData();
+  },
+};
+</script>
+
+<style></style>
